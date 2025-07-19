607ac536865e36b6f65037ba4a348f960c625602
SandeshSS23 <sandeshssahane23@gmail.com>
2025-07-19 21:58:02 +0530

    tested auto news letter genrator locally

--- auto_newsletter.py
+++ auto_newsletter.py
@@ -7,6 +7,6 @@ agent.summarize_articles()
 newsletter_html = agent.compile_newsletter()
 
 # Save and send
-from utils import save_newsletter, send_email
+from app import save_newsletter, send_email
 save_newsletter(newsletter_html)
 send_email("Your AI-Powered Newsletter", "newsletter.html")
